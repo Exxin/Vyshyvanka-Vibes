d70b75623b3c018657ba987802e73e5fda0513b8
Exxin <exxinblood@gmail.com>
2023-11-30 13:42:34 +0200

    a

--- src/js/mob-menu-close.js
+++
@@ -1,6 +0,0 @@
-const closeButton = document.querySelector('.mob-menu-close-btn');
-const menu = document.querySelector('.mob-menu');
-
-closeButton.addEventListener('click', () => {
-  menu.classList.toggle('is-open');
-});
